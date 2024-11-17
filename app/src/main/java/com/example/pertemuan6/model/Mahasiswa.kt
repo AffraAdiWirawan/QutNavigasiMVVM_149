e1de416fe1fd714139a2974c50719bb30b89a2bf
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-11-17 10:27:48 +0700

    Membuat data class mahasiswa

---
+++ app/src/main/java/com/example/pertemuan6/model/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan6.model
+
+data class Mahasiswa(
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = "",
+    val nim : String = "",
+)
